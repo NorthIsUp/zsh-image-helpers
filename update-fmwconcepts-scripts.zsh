153df555aa524866090981ae8c34cdd43a7daa9e
Adam Hitchcock <adam@northisup.com>
2021-07-23 22:02:43 -0400

    update workflow

---
+++ update-fmwconcepts-scripts.zsh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  killall -9 curl
+}
+
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+bin_root="${0:a:h}/bin"
+
+# update scripts list
+curl -O http://www.fmwconcepts.com/imagemagick/script_list.txt
+
+scripts=( $( awk '/./ { print $1 }' script_list.txt ) )
+
+(( $+functions[log-line] )) || function log-line() {
+    print -u2 "$@"
+}
+
+for script in $scripts ; {
+    log-line "downloading $script"
+    curl \
+        --silent \
+        -o $bin_root/im-$script \
+        "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=$script&dirname=$script"
+}
