eed84a5fc6f94c04dab4ba47d44f806bb12bae9c
Adam Hitchcock <adam@northisup.com>
2021-07-23 22:33:57 -0400

    remove debug

--- update-fmwconcepts-scripts.zsh
+++ update-fmwconcepts-scripts.zsh
@@ -1,10 +1,5 @@
 #!/usr/bin/env zsh
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  killall -9 curl
-}
+set -euo pipefail
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
@@ -12,14 +7,9 @@ cleanup() {
 bin_root="${0:a:h}/bin"
 
 # update scripts list
-
-
-scripts=(
-  $(
-    curl http://www.fmwconcepts.com/imagemagick/script_list.txt \
-      | awk '{ print $1 }'
-  )
-)
+scripts=($(
+  curl http://www.fmwconcepts.com/imagemagick/script_list.txt | awk '{ print $1 }'
+))
 
 (( $+functions[log-line] )) || function log-line() {
     print -u2 "$@"
@@ -31,5 +21,4 @@ for script in $scripts ; {
         --silent \
         -o $bin_root/im-$script \
         "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=$script&dirname=$script"
-    break
 }
